3360fe52eef4b9c709528776df5dc9e320fe07e0
apperdog <apperdog@gmail.com>
2018-07-18 01:19:56 +0800

    add

---
+++ Assets/Editor/AI/BaseInputNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EditorMonsterAI
+{
+  public class BaseInputNode : BaseNode
+  {
+    public BaseInputNode(int id) : base(id)
+    {
+
+    }
+  }
+}
+
+
