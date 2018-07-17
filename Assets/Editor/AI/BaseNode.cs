4a40a042d29505de7e6b6af226b841056dd0bf88
apperdog <apperdog@gmail.com>
2018-07-18 00:45:55 +0800

    add

---
+++ Assets/Editor/AI/BaseNode.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace EditorMonsterAI
+{
+  public abstract class BaseNode : ScriptableObject
+  {
+    public int windIndex;  // 窗口ID
+    public string style;
+
+    public Rect windowRect;  // 窗口
+    public Vector2 editorPostion;  // 位置
+    public string windowTitle = "";  // 窗口標題
+
+    public BaseNode(int id)
+    {
+      windIndex = id;
+    }
+
+    public virtual void DrawWindow()
+    {
+
+    }
+
+    public virtual void OnClick()
+    {
+
+    }
+  }
+}
+
